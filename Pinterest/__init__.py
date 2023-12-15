5c678f63246d755210c8965915e7c6a5e6e24a53
romys <romysaputrasihanandaa@gmail.com>
2023-12-15 15:30:55 +0700

    first commits

---
+++ Pinterest/__init__.py
@@ -0,0 +1,1 @@
+from .Pinterest import Pinterest 
--- main.py
+++ main.py
@@ -1,14 +1,27 @@
 import requests
+from json import dumps
 
-url = 'https://www.pinterest.com/resource/BaseSearchResource/get/?source_url=/search/pins/?q=freya&rs=typed&data={"options":{"article":"","appliedProductFilters":"---","price_max":null,"price_min":null,"query":"freya","scope":"pins","auto_correction_disabled":"","top_pin_id":"","filters":"","page_size":"14"},"context":{}}&_=1702623698442'
-
-params = {
-    "source_url":"/search/pins/?q=ashel",
-    "rs": "typed",
-    "data": '{"options":{"article":"","appliedProductFilters":"---","price_max":null,"price_min":null,"query":"ashel","scope":"pins","auto_correction_disabled":"","top_pin_id":"","filters":"","page_size":"14"},"context":{}}',
-    "_": 1702623698442
+data = {
+    "options": {
+        "article": "",
+        "appliedProductFilters": "---",
+        "price_max": None,
+        "price_min": None,
+        "query": "tatto anime",
+        "scope": "pins",
+        "auto_correction_disabled": "",
+        "top_pin_id": "",
+        "filters": "",
+        "page_size": "14"
+    },
+    "context": {}
 }
 
-res = requests.get('https://www.pinterest.com/resource/BaseSearchResource/get', params=params)
+params = {
+    "data": '{"options":{"article":"","appliedProductFilters":"---","price_max":null,"price_min":null,"query":"melody jkt48","scope":"pins","auto_correction_disabled":"","top_pin_id":"","filters":"","page_size":"14"},"context":{}}'
+}
 
+res = requests.get('https://www.pinterest.com/resource/BaseSearchResource/get', params={"data": dumps(data)})
+
+print(len(res.json()['resource_response']['data']['results']))
 print(res.json()['resource_response']['data']['results'].pop()['images'])
