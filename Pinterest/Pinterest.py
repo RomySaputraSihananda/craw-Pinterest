d299d2e059fb83459278e18ded3c89c62a08ca1b
romys <romysaputrasihanandaa@gmail.com>
2023-12-15 20:22:47 +0700

    update

--- Pinterest/Pinterest.py
+++ Pinterest/Pinterest.py
@@ -1,11 +1,19 @@
 import requests
 from requests import Response
 from json import dumps
+from Pinterest.helpers import Datetime
+
 class Pinterest:
     def __init__(self) -> None:
-        self.__url: str = 'https://www.pinterest.com/resource/BaseSearchResource/get'
+        self.__BASE_URL: str = 'https://www.pinterest.com'
+        self.__datetime: Datetime = Datetime()
         self.__result: dict = {}
-    
+        
+        self.__result['date_now']: str = None
+        self.__result['keyword']: str = None
+        self.__result['size']: str = None
+        self.__result['data']: list = []
+
     def __build_params(self, keyword: str, size: int) -> dict:
         return {
             'data': dumps({
@@ -24,16 +32,33 @@ class Pinterest:
                 "context": {}
             })
         } 
+    
+    def __filter_data(self, datas: list) -> None:
+        self.__result['size'] = len(datas)
+
+        for data in datas:
+            self.__result['data'].append({
+                "id": data["id"],
+                "link_pin": f'{self.__BASE_URL}/pin/{data["id"]}',
+                "title": data["title"],
+                "created_at": self.__datetime.execute(data["created_at"]),
+                "link": data["link"]
+            })
 
     def search(self, keyword: str, **kwargs):
-        response: Response = requests.get(self.__url, params=self.__build_params(keyword, kwargs.get('size', 10)))
+        size = kwargs.get('size', 10)
 
+        response: Response = requests.get(f'{self.__BASE_URL}/resource/BaseSearchResource/get', params=self.__build_params(keyword, size)).json()
 
+        self.__result['date_now']: str = self.__datetime.now()
+        self.__result['keyword']: str = keyword
 
-        # with open('test_data.json', 'w') as file:
-        #     file.write(dumps(response.json()['resource_response']['data']['results'], indent=2, ensure_ascii=False))
+        self.__filter_data(response['resource_response']['data']['results'])
+
+        with open('test_data2.json', 'w') as file:
+            file.write(dumps(self.__result, indent=2, ensure_ascii=False))
 
 # testing
 if(__name__ == '__main__'):
     pins = Pinterest = Pinterest()
-    pins.search('freya', size=12)
+    pins.search('freya', size=5)
